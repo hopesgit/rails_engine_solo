99db8048ea0cd3282200a5e34ef82e6da174fcd2
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-04 12:46:22 -0600

    Add merchant validations and spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -10,5 +10,6 @@ describe Invoice do
   describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :merchant }
+    it { should have_many :transactions }
   end
 end
---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Merchant do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :items }
+    it { should have_many :invoices }
+  end
+end
